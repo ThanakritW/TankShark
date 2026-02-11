856bf5618a765b8a04580d062709de1158a4985d
ThanaphomH <thanaphom.h@gmail.com>
2026-02-12 00:30:55 +0700

    fix: bullet wall collision

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,15 +3,23 @@ extends Area2D
 @export var speed := 1000.0
 @export var damage := 1
 
-var direction: Vector2
+var direction: Vector2 = Vector2.RIGHT
 
-func _ready():
-	body_entered.connect(_on_body_entered)
+@onready var shape_cast = $ShapeCast2D
 
 func _physics_process(delta):
-	position += direction * speed * delta
-
-func _on_body_entered(body):
-	if body.has_method("take_damage"):
-		body.take_damage(damage)
-	queue_free()
+	var velocity = direction * speed * delta
+	
+	shape_cast.target_position = to_local(global_position + velocity)
+	shape_cast.force_shapecast_update()
+	
+	if shape_cast.is_colliding():
+		# We hit something
+		var body = shape_cast.get_collider(0)
+		
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
+			
+		queue_free()
+	else:
+		global_position += velocity
